e0794db3bbc417e3b3d90d2eff6395859f28db0b
Yuto Arimori <n01648493@humber.ca>
2024-05-22 20:28:59 -0400

    updated file

--- week3/index.php
+++ week3/index.php
@@ -11,14 +11,29 @@
     // $hour = date('H');
     // echo $hour;
 
-    $hour = rand(1,24);
-    echo $hour;
+    // $hour = rand(1,24);
+    // echo $hour;
 
-    if($hour < 12) {
-        echo "Good morning.";
-    }elseif($hour > 12) {
-        echo "Good evening.";
+    // if($hour < 12) {
+    //     echo "Good morning.";
+    // }elseif($hour >= 12) {
+    //     echo "Good evening.";
+    // }
+
+    $magicNumber = rand(1,100);
+    $fizz = $magicNumber%3;
+    $buzz = $magicNumber%5;
+
+    if ($fizz == 0) {
+        echo "Fizz";
+    }elseif ($buzz == 0) {
+        echo "Buzz";
+    }elseif ($fizz == 0 && $buzz == 0){
+        echo"FizzBuzz";
+    }else {
+       echo $magicNumber;
     }
+    // echo $magicNumber;    
     ?>
 </body>
 </html>
